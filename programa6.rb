1fe9260af15ec28588345974b4085d56299f001c
javacf <flores.orihuela@gmail.com>
2020-06-06 18:22:52 -0500

    02.00

---
+++ programa6.rb
@@ -0,0 +1,37 @@
+=begin
+Un conocido diario ofrece suscripciones anuales a los clientes. 
+Con la finalidad de promover este tipo de venta, el diario ha 
+ofrecido a otras empresas de diverso índole, suscripciones a 
+los trabajadores de dichas empresas con el aval de las mismas.
+ Los precios de las suscripciones son las siguientes:
+-	Suscripción a la revista “Vamos”: 150 nuevos soles Anuales.
+-	Suscripción a la revista “Cosas”: 170 nuevos soles Anuales.
+-	Suscripción a la revista “Automas”: 250 nuevos soles Anuales.
+-	Suscripción a la revista “Construye”: 200 nuevos soles Anuales.
+Adicionalmente, dependiendo de la cantidad de suscripciones se 
+pueden dar descuentos por volumen: 
+-	De 1 a 10 suscripciones, tiene un 0% de descuento.
+-	De 11 a 100 suscripciones, tiene un 15% de descuento.
+-	De 101 a 200 suscripciones, tiene un 21% de descuento.
+-	Más de 200 suscripciones, tiene un 25% de descuento.
+Desarrollar un subprograma que permita determinar el precio de 
+cada suscripción, el porcentaje de descuento dependiendo de la 
+cantidad de suscripciones a adquirir, así como el total que debería 
+pagar la empresa si adquiriera un determinado tipo y número de 
+suscripciones 
+=end
+#encoding:UTF-8
+def calcularPrecioSuscripcion(tipo)
+
+end
+
+def calcularDescuentoCantidad(cantidad)
+
+end
+
+
+def calcularTotalPagar(tipo,cantidad)
+  
+end
+
+#--- zona de test ----
---
+++ programa7.rb
@@ -0,0 +1,44 @@
+=begin
+Una empresa de auto lavado está promocionando los nuevos paquetes de lavado para sus
+clientes fieles. Para esto ha promocionado las nuevas tarifas por el servicio de lavado:
+ 
+Modelo		Precio
+Sedan		5.50
+Camioneta	7.90
+4x4			10.00
+Van			13.90
+ 
+Adicionalmente el cliente puede solicitar que se haga un lavado interno y para ello se le
+agregan las siguientes tarifas:
+ 
+Modelo		Adicional
+Sedan		2.50
+Camioneta	3.90
+4x4			5.00
+Van			7.30
+ 
+Finalmente, si es la quinta vez que el cliente nos visita con el mismo vehículo entonces 
+tiene un descuento de 15% sobre el valor final del servicio de lavado.
+ Se le solicita lo siguiente:
+ 
+1.	Dado un modelo, mostrar el monto a pagar por el servicio de lavado básico.
+2.	Dado un modelo, mostrar el monto a pagar por el servicio de lavado interno.
+3.	Dado un modelo, poder calcular el valor final a pagar considerando si el cliente 
+solicita el servicio de lavado interno y si es que se aplica el descuento por ser cliente
+frecuente. (se tendrá como input un valor boolean por si toma el servicio adicional
+o no y si tiene el descuento)
+=end
+
+def calcularValorLavado(modelo)
+
+end
+
+def calcularValorServicioAdicional(modelo)
+
+end
+
+def calcularValorTotalPagar(modelo, adicional, quintavisita)
+
+end
+
+#--- zona de test ----
